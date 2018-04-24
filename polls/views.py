4f488228fa111da07f94f34fe03cb6f1897e12bb
cherykid <chenrui9551@foxmail.com>
2018-04-25 01:18:50 +0800

    MOD: use rendor method rendor html.

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
 
 from .models import Question
 
@@ -8,12 +7,11 @@ from .models import Question
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     output = ', '.join([q.question_text for q in latest_question_list])
-    template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
         'string_info': output
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
